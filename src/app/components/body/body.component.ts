15b8ae733456372400fc017396ebb7b7a4ae50b2
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-07-30 09:57:59 +0530

    data binding

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DatabindingComponent } from '../databinding/databinding.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [
+    DatabindingComponent
+  ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
---
+++ src/app/components/databinding/databinding.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'app-databinding',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './databinding.component.html',
+  styleUrl: './databinding.component.css'
+})
+export class DatabindingComponent {
+  name = 'Sachin Tendulkar';
+  img_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL0CuKsnxGuq6Fd5AFYt8uaXUwzVbNj5SnGShe5g47-xSU6_713C4KOCTdxy8Njdjz5BY&usqp=CAU';
+  flag = false;
+  userId = 1;
+
+  f1() {
+    console.log('F1........')
+  }
+  changeName() {
+    this.name = 'Sourav Ganguly';
+    this.userId = 10;
+  }
+  onUserIdChange(evt: any) {
+    console.log(evt);
+    // on user id changed , get the details of the updated userid and display on the screen
+  }
+  userIdChanged() {
+    console.log(this.userId,'second')
+  }
+}
